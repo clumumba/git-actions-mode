171cb2bb39f86a5d0c29a1b0898ea3924e8f5040
lumumba95 <cescidy@gmail.com>
2025-06-27 13:58:29 -0500

    commit

--- vpc/var.tf
+++ vpc/var.tf
@@ -10,7 +10,7 @@ variable "availability_zones" {
   
 # }
 
-# the public-cidr-block
+# the public-cidr-block change
 variable "public_cidr_block" {
   description = "CIDR block for the public subnet"
   type        = list(string) # requires the list type to allow multiple CIDR blocks
